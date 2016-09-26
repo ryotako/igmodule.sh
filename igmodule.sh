467351d66f70cfd31403f32f98cc7c5a0fc86d62
ryotako <kobayashiryota2046@gmail.com>
2016-09-27 03:30:13 +0900

    refactoring

--- igmodule.sh
+++ igmodule.sh
@@ -1,131 +1,189 @@
 #!/bin/bash
+PROGNAME=$(basename "$0")
 
-atexit() {
-      [[ -n $tmpfile ]] && rm -f "$tmpfile"
+# Create and kill a temporary file
+function atexit() {
+  if [ -f "$TMPFILE" ]; then
+    rm -f "$TMPFILE"
+  fi
 }
-tmpfile=`mktemp`
 trap atexit EXIT
-trap 'trap - EXIT; atexit; exit -1' SIGHUP SIGINT SIGTERM
+trap 'trap - EXIT; atexit; EXIT 1' HUP INT TERM
+TMPFILE="$(mktemp)"
 
-# variables
-igmodule_progname="$(basename $0)"
-igmodule_proclist=()
-igmodule_modulelist=()
-igmodule_module=""
-igmodule_args=()
+# Print an error message
+function error(){
+  echo "$@" 1>&2
+}
 
-igmodule_sed(){
-  if which -s gsed; then
-    gsed -E "$1i"
+# Show usage and exit
+function usage_exit(){
+  cat << EOS 
+Usage: $PROGNAME [options] [file path]
+
+Options:
+  --help     show this help message and exit
+  --path     show the path of your 'User Procedures' directory
+
+  --as       module name of packaged file
+  --include  You can select a target file as #include style.
+
+Environment variables:
+  IGORPATH   path of your 'Igor Pro User Files' directory
+EOS
+  exit 0
+}
+
+# Find ''User Procedures' directory
+# If it is not found, print error message and exit
+function user_procedures(){
+  local wm_dir="$HOME/Documents/WaveMetrics"
+  local usrdir_igor7="$wm_dir/Igor Pro 7 User Files/User Procedures"
+  local usrdir_igor6="$wm_dir/Igor Pro 6 User Files/User Procedures"
+  local usrdir_igor="$wm_dir/Igor Pro User Files/User Procedures"
+  if [ -n $"IGORPATH" ];then
+    if [ -d "$IGORPATH/User Procedures" ];then
+      local usrdir="$IGORPATH/User Procedures"
+    else
+      error "$PROGNAME: illegal environment variable IGORPATH"
+      error "IGORPATH=$IGORPATH"
+      exit 1
+    fi
+  elif [ -d "$usrdir_igor7" ]; then
+    local usrdir="$usrdir_igor7"
+  elif [ -d "$usrdir_igor6" ]; then
+    local usrdir="$usrdir_igor6"
+  elif [ -d "$usrdir_igor" ]; then
+    local usrdir="$usrdir_igor"
   else
-    sed -E "$1"
+    error "$PROGNAME: your 'User Procedures' directory is not found"
+    error "Set environment variable IGORPATH"
+    exit 1
+  fi
+  echo "$usrdir"
+}
+
+# Find .ipf file from procedure name and return its file path
+# #include "procname" <- procedure name is this
+function find_procedure(){
+  local proc_name="$1"
+  local root_file="$2"
+
+  function convert_igorpath(){
+    echo "$1.ipf" | sed -E \
+      -e 's/^Macintosh HD:/\//' \
+      -e 's/::/:\.\.:/g' \
+      -e 's/:/\//g'
+  }
+  if [[ "$proc_name" =~ ^: ]]; then # Relative path
+    echo $(dirname "$root_file")$(convert_igorpath "$proc_name") 
+  elif [[ "$proc_name" =~ : ]]; then # Absolute path
+    echo $(convert_igorpath "$proc_name")
+  else # Procedure name only
+    find "$(user_procedures)" -type f -name "$proc_name.ipf" | head -n 1
   fi
 }
 
-contain(){
-  local elm="$1" 
+# Parse options
+while (( $# > 0 ))
+do
+  case "$1" in
+    --help )
+      usage_exit
+      ;;
+    --as )
+      OPT_AS="$2"
+      shift
+      ;;
+    --include )
+      OPT_INCLUDE=1
+      ARGS+=$(find_procedure "$2")
+      shift
+      ;;
+    --path )
+      user_procedures
+      exit 0
+      ;;
+    --* )
+      error "$PROGNAME: illegal option $1"
+      exit 1
+      ;;
+    * )
+      ARGS+=("$1")
+      ;;
+  esac
   shift
-  local array=($@)
-  local i=0
-  for elm_i in ${array[@]}; do
-    if [ "$elm_i" = "$elm" ];then
+done
+
+# Check the number of arguments
+if [ ${#ARGS[@]} -lt 1 ]; then
+  error "$PROGNAME: too few arguments"
+  error "Usage: $PROGNAME [options] [file path]"
+  exit 1
+elif [ ${#ARGS[@]} -gt 1 ]; then
+  if [ -n "$OPT_INCLUDE" ]; then
+    error "$PROGNAME: too many arguments"
+    error "When you use --include option, you cannot select another file"
+  else
+    error "$PROGNAME: too many arguments"
+    error "Usage: $PROGNAME [options] [file path]"
+  fi
+  exit 1
+fi
+ARG="${ARGS[0]}"
+
+# Determines whether $1  may be found within an array
+function contains(){
+  local arg="$1" 
+  shift
+  local array=("$@")
+  local item
+  for item in "${array[@]}"
+  do
+    if [ "$item" = "$arg" ];then
       return 0
     fi
-    let i++
   done
   return 1
 }
 
-# Find User Procedures
-user_proc_dir(){
-  if [ -n $"IGORPATH" ];then
-    echo $IGORPATH
-  else
-    find "$HOME/Documents/WaveMetrics" -type d -name 'Igor Pro *User Files' \
-      | tail -n 1
-  fi
-}
-
-# Find a procedure
-find_procedure(){
-  proc_name="$1"
-  file_path="$2"
-
-  # Change : into / in an igor-path
-  convert_igorpath(){
-    echo "$1.ipf" | sed -e 's/^Macintosh HD://' -e 's/^[^\/]/\//' | tr ':' '/' 
-  }
-
-  if [[ "$proc_name" =~ ^: ]]; then # Absolute path
-    echo $(dirname "$file_path")$(convert_igorpath "$proc_name")
-  elif [[ "$proc_name" =~ : ]]; then # Relative path
-    convert_igorpath "$proc_name"
-  else # File name only
-    find "$(user_proc_dir)/User Procedures" -type f -name "$proc_name.ipf" \
-      | head -n 1 
-  fi
-}
-
-# Return packaged text
-pack_procedures(){
-  local proc_path="$1"
-  local proc_name=$(basename "$1" .ipf)
-  igmodule_module="${igmodule_module:-$proc_name}"
-  cat << EOS
-//------------------------------------------------------------------------------
-// This procedure file is packaged by $igmodule_progname
-// $(LANG=en_US.UTF-8 date '+%a,%d %b %Y')
-//------------------------------------------------------------------------------
-#pragma ModuleName=$igmodule_module
-
-EOS
-
-  pack_procedures_recursive "$proc_path" > "$tmpfile"
-
-  rewrite_modulecall < "$tmpfile"   
-  
-}
-
-rewrite_modulecall(){
-  local module="${igmodule_modulelist[0]}"
-  igmodule_modulelist=("${igmodule_modulelist[@]:1}")
-  if [ "${#igmodule_modulelist[@]}" -eq 0 ]; then
-    cat -
-  else
-    cat - | igmodule_sed "s/$module#/$igmodule_module#/g" | rewrite_modulecall
-  fi
-}
-
-
-# Search included files recursively
-pack_procedures_recursive(){
-  local proc_path="$1"
-  local proc_name=$(basename "$1" .ipf)
-  if contain "$proc_name" "${igmodule_proclist[@]}";then
-    return 1
-  fi
-
-  pack_procedure "$proc_path"
-  igmodule_proclist+=("$proc_name")
-  igmodule_modulelist+=( $(egrep '^#pragma +ModuleName' "$proc_path" \
-    | igmodule_sed 's/^[^=]*= *([^ ]+).*$/\1/' | head -n 1) )
-  while read pragma; do
-    local included_proc="$(echo $pragma \
-      | sed -E 's/^#include +\"([^\"]+)\".*$/\1/')"
-    local included_path=$(find_procedure "$included_proc" "$proc_path")
-    if [ -n "$included_path" ]; then
-        pack_procedures_recursive "$included_path"
+# convert: Case? -> [Ca][Aa][Ss][Ee]?
+# BSD sed cannot ignore cases :(
+function ignorecase_pattern(){
+  local pattern="$1"
+  if [ -n "$pattern" ]; then
+    local char=$(echo "$pattern" | cut -c 1)
+    local chars=$(echo "$pattern" | cut -c 2-)
+    if [[ $char =~ [a-z] ]];then
+      local upper=$(echo "$char" \
+        | sed 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/')
+      local head="[$char$upper]"
+    elif [[ "$char" =~ [A-Z] ]];then
+      local lower=$(echo "$char" \
+        | sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/')
+      local head="[$lower$char]"
     else
-      echo "Not Found: $included_proc" 1>&2
+      local head="$char"
     fi
-  done < <(egrep '^#include +"[^"]+"' "$proc_path")
+    local tail=$(ignorecase_pattern "$chars")
+    echo "$head$tail"
+  fi
 }
 
-# Rewrite an included file
-# TODO include option, menus=0
-pack_procedure(){
+REGEXP_INCLUDE=$(ignorecase_pattern '^#include +')
+REGEXP_MUDULE_NAME=$(ignorecase_pattern '^#pragma ModuleName')
+REGEXP_CONSTANT=$(ignorecase_pattern '^(constnat) ')
+REGEXP_STRCONSTANT=$(ignorecase_pattern '^(strconstnat) ')
+REGEXP_FUNCTION=$(ignorecase_pattern '^(Function) ')
+
+# Rewrite procedure file
+function rewrite_procedure(){
   local proc_path="$1"
-  local proc_name=$(basename "$1" .ipf)
+  local module_name="$2"
+
+  local proc_name=$(basename "$proc_path" .ipf)
+
+  # header
   cat <<EOS
 //------------------------------------------------------------------------------
 // original file: $proc_name.ipf 
@@ -133,55 +191,100 @@ pack_procedure(){
 #if !ItemsInList(WinList("$proc_name.ipf",";",""))
 
 EOS
+  # body
+  cat "$proc_path" | sed -E \
+    -e "/$REGEXP_INCLUDE/ s/^/\/\//" \
+    -e "/$REGEXP_MUDULE_NAME/ s/^/\/\//" \
+    -e "s/$REGEXP_CONSTANT/override \1 /" \
+    -e "s/$REGEXP_STRCONSTANT /override \1 /" \
+    -e "s/$REGEXP_FUNCTION/override \1 /" \
 
-  cat "$proc_path" \
-    | igmodule_sed '/^#include +"/ s/^/\/\//' \
-    | igmodule_sed '/^#pragma ModuleName/ s/^/\/\//' \
-    | igmodule_sed 's/^constant /override constant /' \
-    | igmodule_sed 's/^strconstant /override strconstant /' \
-    | igmodule_sed 's/^Function/override Function/' \
-
+  # tail
 cat <<EOS
 
 #endif
 
 EOS
 }
+# Rewrite module calls: ModuleName#Function 
+function rewrite_modulecall(){
+  local module_name="$1"
+  shift
+  local included_modules=("$@")
 
-
-# Show help and exit
-usage_exit(){
-  echo "Usage: $(basename $0) [--as module] [--include proc]" 1>&2
-  exit 1
+  if [ "${#included_modules[@]}" -lt 1 ]; then
+    cat -
+  else
+    local pattern=$(ignorecase_pattern "${included_modules[0]}")
+    included_modules=("${included_modules[@]:1}")
+    cat - | sed -E "s/$pattern#/$module_name#/g" \
+      | rewrite_modulecall "$module_name" "${included_modules[@]}"
+  fi
 }
 
-# Parse options
-while (( $# > 0 )); do
-  case $1 in
-    --help)
-      usage_exit
-      ;;
-    --as)
-      igmodule_module="$2"
-      shift 2
-      ;;
-    --include)
-      igmodule_args=("$(find_procedure $2 '')")
-      shift 2
-      ;;
-    *)
-      igmodule_args+=("$@")
-      break
-      ;;
-  esac
-done
+# Search included file recursively
+function pack_procedures_recursive(){
+  local proc_path="$1"
+  local module_name="$2"
 
-argc="${#igmodule_args[@]}"
-if [ "$argc" -lt 1 ]; then
-  echo "$igmodule_progname: too few arguments" 1>&2
-elif [ "$argc" -gt 1 ]; then
-  echo "$igmodule_progname: too many arguments" 1>&2
-else
-  pack_procedures "${igmodule_args[0]}"
-fi
+  local proc_name=$(basename "$proc_path" .ipf)
+  if contains "$proc_name" "${included_procs[@]}" ; then
+    return 0
+  fi
+  rewrite_procedure "$proc_path" "$module_name"
+  
+  # record a leoaded procedure name
+  included_procs+=("$proc_name")
 
+  # record a loaded module name
+  local included_module=$(egrep -i '^#pragma +ModuleName *=' "$proc_path" \
+    | sed -E 's/^[^=]+= *([a-zA-Z_][a-zA-Z_0-9]*).*$/\1/' | head -n 1)
+  if [ -n "$included_module" ];then
+    included_modules+=("$included_module")
+  fi
+
+  local pragma=''
+  while read pragma
+  do
+    local included_proc=$(echo "$pragma" | sed -E 's/^[^"]+"([^"]+)".*$/\1/')
+    local included_path=$(find_procedure "$included_proc" "$proc_path")
+    if [ -f "$included_path" ]; then
+      pack_procedures_recursive "$included_path" "$module_name"
+    else
+      error "$PROGNAME: $pragma: not found"
+    fi
+  done < <(egrep -i '^#include +"[^"]+"' "$proc_path")
+}
+
+# First step of packaging
+# Find file by path and add header
+# if it is not found, exit.
+function pack_procedures(){
+  local proc_path="$1"
+  local module_name="$2"
+  if [ ! -f "$proc_path" ]; then
+    error "$PROGNAME: $included_proc: no such file"
+    exit 1
+  fi
+
+  local proc_name=$(basename "$1" .ipf)
+  if [ -z "$module_name" ]; then
+    module_name="$proc_name"
+  fi
+  cat << EOS
+//------------------------------------------------------------------------------
+// This procedure file is packaged by $igmodule_progname
+// $(LANG=en_US.UTF-8 date '+%a,%d %b %Y')
+//------------------------------------------------------------------------------
+#pragma ModuleName=$module_name
+
+EOS
+
+  local included_procs=()
+  local included_modules=()
+  pack_procedures_recursive "$proc_path" "$module_name" > "$TMPFILE"
+  rewrite_modulecall "$module_name" "${included_modules[@]}" < "$TMPFILE"
+}
+
+# Execute
+pack_procedures "$ARG" "$OPT_AS" 
